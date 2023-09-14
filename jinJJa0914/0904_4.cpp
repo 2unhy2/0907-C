db17e7030c2eb7d91cc47cbbaa24493f81ce114a
2unhy2 <144293456+2unhy2@users.noreply.github.com>
2023-09-14 10:56:57 +0900

    year22

--- jinJJa0914/0904_4.cpp
+++ jinJJa0914/0904_4.cpp
@@ -5,12 +5,14 @@ int main(void) {
 	printf("연도입력");
 	scanf_s("%d", &year);
 
-	year = (year % 4==0) &&(year % 100!=0)&&(year%400==0);
-	if (year = 0) {
-		printf("윤년입니다.");
-	}
-	else if (year = 1) {
+	int yy = (year % 4==0) &&(year % 100!=0)||(year%400==0);
+	printf("%d", yy);
+	if (yy == 0) {
 		printf("윤년이아닙니다.");
+	}
+	else if (yy == 1) {
+		printf("윤년입니다.");
+		
 	}
 	return 0;
 }
