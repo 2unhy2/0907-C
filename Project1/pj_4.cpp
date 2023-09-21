6a914c8ee6217c2bad5aab3d09111fe8cb8facbd
yujin65 <144310841+asdfsasdfasdf@users.noreply.github.com>
2023-09-21 11:20:33 +0900

    monte

--- Project1/pj_4.cpp
+++ Project1/pj_4.cpp
@@ -6,5 +6,32 @@
 int main(void) {
 	double x = 0;
 	double y = 0;
+	int count = 0, circle = 0;
+	int percent = 0;
+	//double pi;
+	srand(time(NULL));
+
+	while (count < 100000) {
+		x = (double)rand() / (double)RAND_MAX;
+		y = (double)rand() / (double)RAND_MAX;
+		count++;
+
+		if ((x * x) + (y * y) <= 1){
+			circle++;
+		}
+		percent = count / 1000;
+		//pi = ;
+		printf("%d%%진행... pi = %lf: %", percent, (circle / count) * 4);
+		printf("\n");
+	}
 	return 0;
+
 }
+
+/* 몬테카를로 알고리즘 : 랜덤함수생성(100000돌리기)후, 무작위 난수를 기반으로 생성해서 구하고자 하는
+정보의 확률을 계산하는 알고리즘
+x,y에 랜덤함수를 써서 일정한 개수의 점을 균일하게 분포..?
+부채꼴의 넓이 pi/4
+pi/4=점갯수/전체넓이 -> pi=(4*m)/n
+x^+y^<=1 이면 부채꼴 안에 있는 점
+*/
