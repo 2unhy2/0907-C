f0b24a75c493d409193e24e5ad558fd33d7bc8d6
2unhy2 <144293456+2unhy2@users.noreply.github.com>
2023-10-19 10:09:50 +0900

    resocre

--- 1019/sil_1.cpp
+++ 1019/sil_1.cpp
@@ -1,19 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+
 int main() {
 	int* score;
 	int count = 0;
 	int sum = 0;
-	int p = 0;
+	float p = 0;
 	
-	printf("학생 수 입력");
+	printf("학생 수 입력 : ");
 	scanf_s("%d", &count);
 	score = (int*)malloc(10 * sizeof(int));
 
 
 
 	for (int i = 0;i < count;i++) {
+		
 		printf("학생 # %d-%d 성적 입력", count, i+1);
 		scanf_s("%d", score+i);
 		sum += score[i];
@@ -21,12 +23,12 @@ int main() {
 	printf("총점 : %d\n", sum);
 
 	for (int k = 0;k < count;k++) {
-		printf("학생 # %d-%d 성적 출력 : %d \n ", count, k,score);
+		printf("학생 # %d-%d 성적 출력 : %d \n ", count, k+1,score[k]);
 
 
 	}
-	p = sum / count;
-	printf("평균 점수 : %d", p);
+	p = float(sum) / count;
+	printf("평균 점수 : %.2f", p);
 	free(score);
 
 	return 0;
